351fefe8f9d99836d9b30b0a8be8262fd8f16db4
mark <marcmumo@gmail.com>
2018-12-10 14:05:44 +0300

    [finishes #162539400]add a moderator class

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 import datetime
 
-class User:  
-    
+class User:
+
 
     def __init__ (self, name):
         self._name = name
@@ -22,7 +22,7 @@ class User:
     @name.setter
     def name(self, value):
         self._name = value
-    
+
     def is_logged_in(self):
         return self._is_logged_in
 
@@ -38,13 +38,13 @@ class User:
     def log_out(self):
         self._is_logged_in = False
         print('{} has successfully logged out'.format(self._name))
-        
+
     def can_edit(self, comment):
         self._comment = comment
-        if self._comment.author.name == self._name:        
+        if self._comment.author.name == self._name:
             self._can_edit = True
         else:
-            self._can_edit = False          
+            self._can_edit = False
         return self._can_edit
 
     def can_delete(self, comment):
@@ -61,3 +61,17 @@ class User:
     def to_string(self):
         return self.__str__()
 
+class Moderator(User):
+
+    def __init__(self, name):
+        super(moderator, self).__init__(name)
+        self._name = name
+
+    def can_edit(self, comment):
+        if comment.author == self.name: return True
+        return False
+
+    def can_delete(self, comment):
+        self._comment = comment
+        self._can_delete = True
+        return self._can_delete
